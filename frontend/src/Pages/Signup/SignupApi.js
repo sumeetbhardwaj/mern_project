3b8a320e135f7b0808a8078f830b5ba58bd7bab7
Sumeet kumar <sumeet.kumar@codebrewinnovations.com>
2024-03-17 19:32:49 +0530

    ini project

--- frontend/src/Pages/Signup/SignupApi.js
+++ frontend/src/Pages/Signup/SignupApi.js
@@ -3,7 +3,7 @@ import { API_URL } from "../../config";
 
 export const SignupApi = async(values) => {
     try {    
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/signup`,JSON.stringify(values));
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/signup`,values);
         return await response.data; 
       } catch (error) {
         return error;
