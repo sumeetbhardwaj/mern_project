4dd77313a95f458ef112c4af3d3c992ea64bda8f
Sumeet kumar <sumeet.kumar@codebrewinnovations.com>
2024-03-17 19:25:55 +0530

    ini project

--- frontend/src/Pages/Home/LoginApi.js
+++ frontend/src/Pages/Home/LoginApi.js
@@ -3,7 +3,7 @@ import { API_URL } from "../../config";
 
 export const LoginApi = async(values) => {
     try {    
-        const response = await axios.post(`${API_URL}/signin`,values);
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/signin`,values);
         return await response.data; 
       } catch (error) {
         return error;
