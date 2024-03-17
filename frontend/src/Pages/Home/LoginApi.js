e1dd091759e79bcee58846c031ee5a74311ba9fd
Sumeet kumar <sumeet.kumar@codebrewinnovations.com>
2024-03-17 19:55:51 +0530

    ini project

--- frontend/src/Pages/Home/LoginApi.js
+++ frontend/src/Pages/Home/LoginApi.js
@@ -4,7 +4,7 @@ import { API_URL } from "../../config";
 export const LoginApi = async(values) => {
     try {    
         const response = await axios.post(`${process.env.REACT_APP_API_URL}/signin`,values);
-        return await response.json().data; 
+        return await response.data; 
       } catch (error) {
         return error;
       }
--- frontend/src/utils/storage.js
+++ frontend/src/utils/storage.js
@@ -1,7 +1,7 @@
 
 const storage = {
 	getToken: () => {
-		return window.localStorage.getItem("token") ;
+		return window.localStorage.getItem("token");
 	},
 	setToken: (token) => {
 		window.localStorage.setItem("token", JSON.stringify(token));
