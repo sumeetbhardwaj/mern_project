8f8097573088a4ce5b5419b810b46c6c803827d2
Sumeet kumar <sumeet.kumar@codebrewinnovations.com>
2024-03-17 19:43:26 +0530

    ini project

--- frontend/src/Pages/Home/LoginApi.js
+++ frontend/src/Pages/Home/LoginApi.js
@@ -4,7 +4,7 @@ import { API_URL } from "../../config";
 export const LoginApi = async(values) => {
     try {    
         const response = await axios.post(`${process.env.REACT_APP_API_URL}/signin`,values);
-        return await response.data; 
+        return await response.json().data; 
       } catch (error) {
         return error;
       }
--- frontend/src/Pages/Signup/SignupApi.js
+++ frontend/src/Pages/Signup/SignupApi.js
@@ -4,7 +4,7 @@ import { API_URL } from "../../config";
 export const SignupApi = async(values) => {
     try {    
         const response = await axios.post(`${process.env.REACT_APP_API_URL}/signup`,values);
-        return await response.data; 
+        return await response.json().data; 
       } catch (error) {
         return error;
       }
