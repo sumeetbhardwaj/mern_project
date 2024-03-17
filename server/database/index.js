32d5678b236b1f784b53edb1a2b6ec3c73f62bcb
Sumeet kumar <sumeet.kumar@codebrewinnovations.com>
2024-03-17 18:24:53 +0530

    ini project

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,9 @@
 const mongoDB = require('mongoose');
 exports.mongodb = async () => {
     mongoDB.connect(process.env.HOST_URI)
-          .then(() => { console.log("connection okay") })
+          .then(() => { console.log("connection okay") }).catch((error) => {
+            console.error("MongoDB connection error:", error);
+            // Optionally, you might want to throw the error to indicate that the connection failed
+            throw error;
+        })
 }
